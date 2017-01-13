a861416b966497c4dd73f8c762d025a27fe98545
nickc <nickcortale@gmail.com>
2017-01-13 13:53:20 -0500

    forgot to add scipy to the config.py and environment.yml

--- docs/conf.py
+++ docs/conf.py
@@ -28,7 +28,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['numpy', 'numba','scikit-learn']
+MOCK_MODULES = ['numpy', 'numba','scikit-learn', 'scipy']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 # -- General configuration ------------------------------------------------
 
